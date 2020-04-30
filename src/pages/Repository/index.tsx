697ef05687934e72193d2665fcb2d770de0c1d32
Johnny <johnnywevany@outlook.com>
2020-04-30 13:49:25 -0300

    need add repo details

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './style';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://cdn.pixabay.com/photo/2012/11/21/17/02/lion-66898_960_720.jpg"
+            alt="Facebook/react"
+          />
+          <div>
+            <strong>facebook/react</strong>
+            <p>description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aaa">
+          <div>
+            <strong>aaaa</strong>
+            <p>aaa</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
+
+export default Repository;
